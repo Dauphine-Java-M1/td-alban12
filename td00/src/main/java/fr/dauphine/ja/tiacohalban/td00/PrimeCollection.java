87f5b45485d22a813447af1ad311b62a4f862633
tiacal19 <tiacal19@b048-19.crio.dauphine.fr>
2019-09-30 14:23:15 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/tiacohalban/td00/PrimeCollection.java
@@ -0,0 +1,53 @@
+package fr.dauphine.ja.tiacohalban.td00;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	static ArrayList<Integer> numbers = new ArrayList<Integer>();
+	//Constructeur par défaut
+	/*public PrimeCollection()
+	{
+		numbers = new ArrayList<Integer>();
+	}*/
+	//Main
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour!" );
+        initRandom(100,1000);
+        printPrimes(numbers);
+    }
+    //Méthodes 
+	public static void initRandom(int n, int m)
+	{
+		for (int i=0;i<n;i++)
+		{
+			Random rand = new Random();
+			int nombreAleatoire = rand.nextInt(m);
+			numbers.add(nombreAleatoire);
+		}
+	}
+	private static boolean isPrime(int p)
+	{
+		boolean isprime=true;
+		double rest;
+		for (int x=2;x<Math.sqrt((float)(p));x++)
+		{
+			rest=(float)p%(float)x;
+		if (rest==0)
+			isprime=false;
+		}
+		return isprime;
+	}
+	private static void printPrimes(ArrayList<Integer> pnumb)
+	{
+		for (Integer numb : pnumb)
+		{
+			if (isPrime(numb))
+				System.out.println(numb);
+		}
+	}
+}
